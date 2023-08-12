58eb98663896745c6dd2375acbc1b1ee2912799c
Afraz33 <afraz3301@gmail.com>
2023-08-12 16:15:20 +0500

    Progress component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-
+import Progress from "./components/Progress";
 function App() {
   return (
     <div className="app">
       <Header />
+      <Progress />
     </div>
   );
 }
---
+++ src/components/Progress.js
@@ -0,0 +1,5 @@
+function Progress() {
+  return <progress max="100" value="90"></progress>;
+}
+
+export default Progress;
