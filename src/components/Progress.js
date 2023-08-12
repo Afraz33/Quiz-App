bddb2557840cbc64c4dc838d9f19391f709848b2
Afraz33 <afraz3301@gmail.com>
2023-08-12 21:48:10 +0500

    Testing

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -3,7 +3,7 @@ function Progress() {
     <header className="progress">
       <progress max="100" value="90"></progress>
       <p>
-        Question <strong>1</strong>/10
+        Question <strong>1</strong>/20
       </p>
       <p>
         Correct <strong>2</strong>/10
